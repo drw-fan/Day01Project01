65917c5b08b979e04c76ca42d640af609b065afc
Katie <Katie@Katie-PC>
2019-01-31 23:39:37 -0500

    Advent of Code Day 1 Project 1

---
+++ src/FrequencyMain.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FrequencyMain {
+	public static void main(String[] args) {
+		int inputInt;
+		String stringInt;
+		int finalFrequency=0;
+
+		try {
+			File myFile = new File("frequency.txt");
+			Scanner inputFile = new Scanner(myFile);
+
+			while (inputFile.hasNext()) {
+				stringInt = inputFile.nextLine();
+				inputInt = Integer.parseInt(stringInt);
+
+				finalFrequency += inputInt;
+			}
+
+			inputFile.close();
+
+		}
+
+		catch (FileNotFoundException e) {
+			System.out.println("File IO error!");
+		}
+
+		System.out.println(finalFrequency);
+
+	}
+}
